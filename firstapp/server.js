3c643a3aa3e2c1585fadc1c5974cab0f2087a8b4
aurore rigault <rigault.aurore@gmail.com>
2022-12-20 14:47:39 +0100

    asynchronous

--- firstapp/server.js
+++ firstapp/server.js
@@ -1,13 +1,12 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
+function onRequest(request, response) {
+  console.log("Request received.");
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
-
-function execute(someFunction, value) {
-  someFunction(value);
 }
 
-execute(function(word){ console.log(word) }, "Hello");
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started.");
