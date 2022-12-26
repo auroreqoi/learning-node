752fe6c59c01421e2e2c823e7d0de8cb409b1c34
aurore rigault <rigault.aurore@gmail.com>
2022-12-26 17:33:12 +0100

    handler handle like in C & Ruby

--- firstapp/router.js
+++ firstapp/router.js
@@ -1,9 +1,10 @@
 function route(handle, pathname) {
-  console.log("About to route a request for" + pathname);
+  console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === 'function') {
     handle[pathname]();
   } else {
-    console.log("No request handler found for" + pathname);
+    console.log("No request handler found for " + pathname);
+    return "404 Not found";
   }
 }
 
